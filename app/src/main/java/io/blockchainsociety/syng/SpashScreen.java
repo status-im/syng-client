b1ad03e55cffe9fb2a83a120bc7315f618fc46bb
Adrian Tiberius <adrian.tiberius@gmail.com>
2015-07-28 13:29:41 +0200

    Added splash screen.

---
+++ app/src/main/java/io/blockchainsociety/syng/SpashScreen.java
@@ -0,0 +1,52 @@
+package io.blockchainsociety.syng;
+
+import io.blockchainsociety.syng.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class SpashScreen extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash_screen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SpashScreen.this, MainActivity.class);
+                SpashScreen.this.startActivity(mainIntent);
+                SpashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+
+    }
+
+}
