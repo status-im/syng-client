97209c73be5f67763acfb5f9b95c0b4958c29df3
Yaroslav Dmytrotsa <dmytyar@vitgroup.com>
2015-08-11 08:53:35 +0300

    missed method

--- app/src/main/java/io/syng/fragment/WebViewFragment.java
+++ app/src/main/java/io/syng/fragment/WebViewFragment.java
@@ -327,47 +327,10 @@ public class WebViewFragment extends Fragment {
         }
     }
 
-
-
-/*
-    private static final String HTTP_TRUSTDAVIS_METEOR_COM = "http://trustdavis.meteor.com";
-
-    public WebViewFragment() {
-    }
-
-    @SuppressLint("SetJavaScriptEnabled")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_web_view, container, false);
-
-        ObservableWebView webView = (ObservableWebView) view.findViewById(R.id.web_view);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-//
-////        webView.setWebChromeClient(new WebChromeClient() {
-////            public boolean onConsoleMessage(@NonNull ConsoleMessage cm) {
-////
-////                String output = cm.message() + " -- From line "
-////                        + cm.lineNumber() + " of "
-////                        + cm.sourceId();
-////                MainActivity.CONSOLE_LOG += output;
-////                Log.d("SyngJs", output);
-////                return true;
-////            }
-////        });
-        webView.loadUrl(HTTP_TRUSTDAVIS_METEOR_COM);
-        Activity tmp = getActivity();
-        tmp.getWindow().closeAllPanels();
-        return view;
+    public void onSaveInstanceState(Bundle outState)
+    {
+        cordovaInterface.onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
     }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        RefWatcher refWatcher = SyngApplication.getRefWatcher(getActivity());
-        refWatcher.watch(this);
-    }
-*/
 }
