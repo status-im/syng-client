2daaca40f0511eec99c0d2dbf6cdbf28ceec53ca
Jarrad Hope <me@jarradhope.com>
2015-09-02 13:42:52 +0200

    apply MPL2.0 license

--- app/src/main/java/io/syng/adapter/helper/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/io/syng/adapter/helper/SimpleItemTouchHelperCallback.java
@@ -14,6 +14,14 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright (c) 2015 Jarrad Hope
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package io.syng.adapter.helper;
 
 import android.support.v7.widget.RecyclerView;
